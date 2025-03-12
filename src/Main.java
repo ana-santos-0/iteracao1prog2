b1f16c9a5fd0d8ead20b4067a88ac264707f1bd6
ana-santos-0 <up202407704@up.pt>
2025-03-12 16:32:04 +0000

    Primeiras alterações

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
+
+ public static void calcularValores (){
+     System.out.println("Bem vindo ao programa!");
+     System.out.println("Insira o valor de FC");
+     Scanner sc = new Scanner(System.in);
+     double FC = sc.nextDouble();
+     System.out.println("Insira o valor de Saturação");
+     double Sat = sc.nextDouble();
+     System.out.println("Insira o valor de temperatura");
+     double Temp = sc.nextDouble();
+ }
+ public static void verificarDados(double FC, double Sat, double Temp){
+        if (FC<60 || Sat<80 || Temp<34){ //Ana, por favor verifica estes dados
+            System.out.println("Erro ao verificar dados");
+        }
+        if (FC>100 || Sat>100 || Temp>45){
+            System.out.println("Erro ao verificar dados");
+        }
+ }
+ public static float CalcularMedia (double FC, double Sat, double Temp){
+
+ }
 }
