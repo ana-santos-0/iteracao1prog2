784b7482064e4fb546505dabe0f5d720ef16b8c5
ana-santos-0 <up202407704@up.pt>
2025-03-13 11:54:18 +0000

    Primeiras alterações

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,69 @@
 import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+abstract class Pessoa{
+    protected String nome;
+    protected Date dataNascimento;
 
+    public Pessoa (String nome, Date dataNascimento){
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
     }
 
- public static void calcularValores (){
-     System.out.println("Bem vindo ao programa!");
-     System.out.println("Insira o valor de FC");
-     Scanner sc = new Scanner(System.in);
-     double FC = sc.nextDouble();
-     System.out.println("Insira o valor de Saturação");
-     double Sat = sc.nextDouble();
-     System.out.println("Insira o valor de temperatura");
-     double Temp = sc.nextDouble();
- }
- public static void verificarDados(double FC, double Sat, double Temp){
-        if (FC<60 || Sat<80 || Temp<34){ //Ana, por favor verifica estes dados
-            System.out.println("Erro ao verificar dados");
-        }
-        if (FC>100 || Sat>100 || Temp>45){
-            System.out.println("Erro ao verificar dados");
-        }
- }
- public static float CalcularMedia (double FC, double Sat, double Temp){
+    public String getNome(){
+        return nome;
+    }
 
- }
+    public Date getDataNascimento(){
+        return dataNascimento;
+    }
+}
+
+class Paciente extends Pessoa {
+    private double altura;
+    private double peso;
+    public List<SinalVital> sinaisVitais;
+
+    public Paciente(String nome, Date dataNascimento, double altura, double peso) {
+        super(nome, dataNascimento);
+        this.altura = altura;
+        this.peso = peso;
+        this.sinaisVitais = new ArrayList<>();
+    }
+
+    public void adicionarSinalVital(SinalVital sinal) {
+        sinaisVitais.add(sinal);
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+
+        }
+
+        public static void calcularValores() {
+            System.out.println("Bem vindo ao programa!");
+            System.out.println("Insira o valor de FC");
+            Scanner sc = new Scanner(System.in);
+            double FC = sc.nextDouble();
+            System.out.println("Insira o valor de Saturação");
+            double Sat = sc.nextDouble();
+            System.out.println("Insira o valor de temperatura");
+            double Temp = sc.nextDouble();
+        }
+
+        public static void verificarDados(double FC, double Sat, double Temp) {
+            if (FC < 60 || Sat < 80 || Temp < 34) { //Ana, por favor verifica estes dados
+                System.out.println("Erro ao verificar dados");
+            }
+            if (FC > 100 || Sat > 100 || Temp > 45) {
+                System.out.println("Erro ao verificar dados");
+            }
+        }
+
+        /*public static float CalcularMedia(double FC, double Sat, double Temp) {
+            return;
+        }
+        */
+         */
+    }
 }
