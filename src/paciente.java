e765da17836132add2ebe694a1f24d7c6a6bfdb6
anaesperanca <up201503147@up.pt>
2025-03-13 15:49:26 +0000

    Update paciente.java

--- src/paciente.java
+++ src/paciente.java
@@ -1,23 +1,61 @@
 public class Paciente {
-    private int frequenciaCardiaca;
-    private double temperatura;
-    private int saturacao;
+    private String nome;
+    private String dataNascimento;
+    private double altura;
+    private double peso;
 
-    public Paciente(int frequenciaCardiaca, double temperatura, int saturacao) {
-        this.frequenciaCardiaca = frequenciaCardiaca;
-        this.temperatura = temperatura;
-        this.saturacao = saturacao;
+    public Paciente(String nome, String dataNascimento, double altura, double peso) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.altura = altura;
+        this.peso = peso;
     }
 
-    public int getFrequenciaCardiaca() {
-        return frequenciaCardiaca;
+    //get
+    public String getNome() {
+        return nome;
     }
 
-    public double getTemperatura() {
-        return temperatura;
+    public String getDataNascimento() {
+        return dataNascimento;
     }
 
-    public int getSaturacao() {
-        return saturacao;
+    public double getAltura() {
+        return altura;
     }
-}
+    public double getPeso(){
+        return peso
+    }
+
+    
+    //Set
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setDataNascimento(String dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+    
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+     public String toString() {
+        return "Paciente{" +
+                "nome='" + nome + '\'' +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+
+
+
+
+
+
+    
