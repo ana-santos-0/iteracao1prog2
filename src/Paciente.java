02d7c71bc360f1b2166a2677aea5f41026de475a
anaesperanca <up201503147@up.pt>
2025-03-31 15:28:54 +0100

    Update Paciente.java

--- src/Paciente.java
+++ src/Paciente.java
@@ -2,16 +2,20 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
 public class Paciente extends Pessoa {
+    private static int idCounter = 0;
+    private int id;
     private double altura;
     private double peso;
     private List<SinalVital> sinaisVitais;
 
     public Paciente(String nome, Date dataNascimento, double altura, double peso) {
         super(nome, dataNascimento);
+        this.id = ++idCounter;
         this.altura = altura;
-        this.sinaisVitais = new ArrayList<>();
         this.peso = peso;
+        this.sinaisVitais = new ArrayList<>();
     }
 
     public void adicionarSinalVital(SinalVital sinal){
@@ -22,14 +26,37 @@ public class Paciente extends Pessoa {
         return sinaisVitais;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
 
     @Override
-     public String toString() {
+    public String toString() {
         return "Paciente{" +
-                "nome='" + nome + '\'' +
-                ", dataNascimento='" + dataNascimento + '\'' +
-                ", altura=" + altura + " m" +
-                ", peso=" + peso + " kg" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", altura=" + altura +
+                ", peso=" + peso +
                 '}';
     }
+
+
 }
