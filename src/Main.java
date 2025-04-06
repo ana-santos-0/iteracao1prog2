82c74ce810b99e7ba921c64e06c6190bdb31636e
ana-santos-0 <up202407704@up.pt>
2025-04-06 21:21:53 +0100

    Alterações finais

--- src/Main.java
+++ src/Main.java
@@ -153,9 +153,4 @@ public class Main {
 
         scanner.close();
     }
-
-    // Corrigido: agora cria a data corretamente
-    private static Date criarData(int ano, int mes, int dia) {
-        return new Date(ano - 1900, mes - 1, dia);
-    }
 }
